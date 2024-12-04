7454596d64e59d47d2326207b471a8277492826b
Yash Doshi <yash.doshi@cctech.co.in>
2024-12-04 11:26:14 +0530

    Change Ttle

--- PaintApp/Temp.cs
+++ PaintApp/Temp.cs
@@ -1,24 +1,23 @@
-﻿//using System.Windows.Controls;
+﻿//using System.Text;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
 //using System.Windows.Input;
-//using System.Windows.Media.Media3D;
 //using System.Windows.Media;
-//using System.Windows.Shapes;
-//using System.Windows;
-
-//using System.Xml.Linq;
-
-//using System;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Input;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
 //using System.Windows.Shapes;
 
-//namespace WpfApplication1
+//namespace PaintApp
 //{
+//    /// <summary>
+//    /// Interaction logic for MainWindow.xaml
+//    /// </summary>
 //    public partial class MainWindow : Window
 //    {
-
+//        bool end = false;
+//        bool start = false;
 //        Point currentPoint = new Point();
 
 //        public MainWindow()
@@ -26,29 +25,96 @@
 //            InitializeComponent();
 //        }
 
-//        private void Canvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+//        private void paintCanvas_MouseDown(object sender, MouseButtonEventArgs e)
 //        {
 //            if (e.ButtonState == MouseButtonState.Pressed)
 //                currentPoint = e.GetPosition(this);
 //        }
 
-//        private void Canvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
+//        private void paintCanvas_MouseMove(object sender, MouseEventArgs e)
 //        {
 //            if (e.LeftButton == MouseButtonState.Pressed)
 //            {
 //                Line line = new Line();
 
-//                line.Stroke = SystemColors.WindowFrameBrush;
-//                line.X1 = currentPoint.X;
-//                line.Y1 = currentPoint.Y;
-//                line.X2 = e.GetPosition(this).X;
-//                line.Y2 = e.GetPosition(this).Y;
+//                SolidColorBrush redBrush = new SolidColorBrush();
+
+//                ComboBoxItem ComboItem1 = (ComboBoxItem)comboBox1.SelectedItem;
+//                string name1 = ComboItem1.Name;
+
+//                ComboBoxItem ComboItem2 = (ComboBoxItem)comboBox2.SelectedItem;
+//                string name2 = ComboItem2.Name;
+
+//                ComboBoxItem ComboItem3 = (ComboBoxItem)drawStyle.SelectedItem;
+//                string name3 = ComboItem3.Name;
+
+//                if (name1 == "one")
+//                {
+//                    line.StrokeThickness = 2;
+//                }
+//                else if (name1 == "two")
+//                {
+//                    line.StrokeThickness = 4;
+//                }
+//                else if (name1 == "three")
+//                {
+//                    line.StrokeThickness = 6;
+//                }
+//                else if (name1 == "four")
+//                {
+//                    line.StrokeThickness = 10;
+//                }
+//                else
+//                {
+//                    line.StrokeThickness = 1;
+//                }
+
+//                if (name2 == "red")
+//                {
+//                    redBrush.Color = Colors.Red;
+//                }
+//                else if (name2 == "black")
+//                {
+//                    redBrush.Color = Colors.Black;
+//                }
+//                else if (name2 == "green")
+//                {
+//                    redBrush.Color = Colors.Green;
+//                }
+
+//                line.Stroke = redBrush;
+
+//                if (name3 == "StraightLine")
+//                {
+//                    if (end)
+//                    {
+//                        line.X1 = currentPoint.X;
+//                        line.Y1 = currentPoint.Y;
+//                        line.X2 = e.GetPosition(this).X;
+//                        line.Y2 = e.GetPosition(this).Y;
+//                    }
+//                }
+//                else if (name3 == "FreeDraw")
+//                {
+//                    line.X1 = currentPoint.X;
+//                    line.Y1 = currentPoint.Y;
+//                    line.X2 = e.GetPosition(this).X;
+//                    line.Y2 = e.GetPosition(this).Y;
+//                }
 
 //                currentPoint = e.GetPosition(this);
 
-//                paintSurface.Children.Add(line);
+//                paintCanvas.Children.Add(line);
 //            }
 //        }
+//        private void paintCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+//        {
+//            end = true;
+//        }
 
+//        private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+//            paintCanvas.Children.Clear();
+//        }
 //    }
 //}
